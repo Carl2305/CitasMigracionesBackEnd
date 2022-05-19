ced9e78a6df4e06a8f1416960cd9ed7ab9646678
Carl2305 <mogollon.23carlos05@gmail.com>
2022-05-19 18:24:48 -0500

    CRUD Recibos

---
+++ src/main/java/com/cita/migraciones/controller/ReciboController.java
@@ -0,0 +1,59 @@
+package com.cita.migraciones.controller;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.cita.migraciones.entitylayer.Recibo;
+import com.cita.migraciones.servicelayer.ReciboService;
+
+@RestController
+@RequestMapping("/api/recibo")
+@CrossOrigin
+public class ReciboController {
+	@Autowired
+	private ReciboService reciboService;
+	
+	@GetMapping("/all")
+	@ResponseBody
+	public ResponseEntity<List<Recibo>> listRecibo(){
+		return new ResponseEntity<>(reciboService.listRecibo(), HttpStatus.OK);
+	}
+	
+	@GetMapping("/{idRecibo}")
+	@ResponseBody
+	public ResponseEntity<Optional<Recibo>> getRecibo(@PathVariable(name = "idRecibo") int idRecibo){
+		return new ResponseEntity<>(reciboService.getRecibo(idRecibo), HttpStatus.OK);
+	}
+	
+	@PostMapping
+	@ResponseBody
+	public ResponseEntity<Recibo> saveRecibo(@RequestBody Recibo recibo){
+		recibo.setIdRecibo(0);
+		return new ResponseEntity<>(reciboService.saveRecibo(recibo), HttpStatus.OK);
+	}
+	
+	@PutMapping
+	@ResponseBody
+	public ResponseEntity<Recibo> updateRecibo(@RequestBody Recibo recibo){
+		return new ResponseEntity<>(reciboService.saveRecibo(recibo), HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/{idRecibo}")
+	@ResponseBody
+	public ResponseEntity<String> deleteRecibo(@PathVariable(name = "idRecibo") int idRecibo){
+		reciboService.deleteRecibo(idRecibo);
+		return new ResponseEntity<>("Recibo Eliminado Correctamente", HttpStatus.OK);
+	}
+}
--- src/main/java/com/cita/migraciones/servicelayer/ReciboServiceImpl.java
+++ src/main/java/com/cita/migraciones/servicelayer/ReciboServiceImpl.java
@@ -1,8 +1,42 @@
 package com.cita.migraciones.servicelayer;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.cita.migraciones.entitylayer.Recibo;
+import com.cita.migraciones.repository.ReciboRepository;
 
 @Service
-public class ReciboServiceImpl {
+public class ReciboServiceImpl implements ReciboService {
+	
+	@Autowired
+	private ReciboRepository repository;
+
+	@Override
+	public List<Recibo> listRecibo() {
+		return repository.findAll();
+	}
+
+	@Override
+	public Optional<Recibo> getRecibo(int idRecibo) {
+		return repository.findById(idRecibo);
+	}
+
+	@Override
+	public Recibo saveRecibo(Recibo recibo) {
+		return repository.save(recibo);
+	}
+
+	@Override
+	public Recibo updateRecibo(Recibo recibo) {
+		return repository.save(recibo);
+	}
+
+	@Override
+	public void deleteRecibo(int idRecibo) {
+		repository.deleteById(idRecibo);
+	}
 
 }
