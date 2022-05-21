fc3a9aebafab42e34ba5a9079b45ee1f1f8ea147
CARLOS MOGOLLON ESPINOZA <mogollon.23carlos05@gmail.com>
2022-05-21 13:56:38 -0500

    actualización de inserts de Sedes
    actualización de inserts de Sedes por orden alfabético

--- SQL/migraciones.sql
+++ SQL/migraciones.sql
@@ -25,9 +25,15 @@ USE sistema_migraciones_2022;
 
 INSERT INTO sede (nombre) VALUES ("Áncash - Chimbote");
 INSERT INTO sede (nombre) VALUES ("Arequipa - Arequipa");
+INSERT INTO sede (nombre) VALUES ("Cajamarca - MAC Real Plaza Cajamarca");
+INSERT INTO sede (nombre) VALUES ("Callao - MAC Mall Plaza Bellavista");
 INSERT INTO sede (nombre) VALUES ("La Libertad - Trujillo");
 INSERT INTO sede (nombre) VALUES ("Lambayeque - Chiclayo");
 INSERT INTO sede (nombre) VALUES ("Lima - Breña");
+INSERT INTO sede (nombre) VALUES ("Lima Este - MAC Agustino Plaza");
+INSERT INTO sede (nombre) VALUES ("Lima Norte - MAC Mall Plaza Comas");
+INSERT INTO sede (nombre) VALUES ("Lima Sur - MAC Open Plaza Atocongo");
+INSERT INTO sede (nombre) VALUES ("Loreto - MAC Enapu Loreto");
 INSERT INTO sede (nombre) VALUES ("Madre de Dios - Puerto Maldonado");
 INSERT INTO sede (nombre) VALUES ("Piura - Piura");
 INSERT INTO sede (nombre) VALUES ("Puno - Puno");
@@ -35,10 +41,5 @@ INSERT INTO sede (nombre) VALUES ("Tacna - Tacna");
 INSERT INTO sede (nombre) VALUES ("Tarapoto - San Martín");
 INSERT INTO sede (nombre) VALUES ("Tumbes - Tumbes");
 INSERT INTO sede (nombre) VALUES ("Ucayali - Pucallpa");
-INSERT INTO sede (nombre) VALUES ("Loreto - MAC Enapu Loreto");
-INSERT INTO sede (nombre) VALUES ("Cajamarca - MAC Real Plaza Cajamarca");
-INSERT INTO sede (nombre) VALUES ("Lima Sur - MAC Open Plaza Atocongo");
-INSERT INTO sede (nombre) VALUES ("Lima Este - MAC Agustino Plaza");
 INSERT INTO sede (nombre) VALUES ("Ventanilla - MAC Ventanilla");
-INSERT INTO sede (nombre) VALUES ("Callao - MAC Mall Plaza Bellavista");
-INSERT INTO sede (nombre) VALUES ("Lima Norte - MAC Mall Plaza Comas");
+
